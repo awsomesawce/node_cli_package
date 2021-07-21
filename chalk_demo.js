0cd8e91c9e4bd2ceff77510f87730364bc0781b1
awsomesawce <awsomesawce@outlook.com>
2021-07-21 18:30:08 +0000

    Install standardx, use it on source files, install ava as dev dep

--- chalk_demo.js
+++ chalk_demo.js
@@ -2,23 +2,22 @@
 // Experimenting with node chalk
 // Taken from node-chalk readme: https://github.com/chalk/chalk#readme
 
-
 const fs = require('fs-extra')
 const path = require('path')
 const chalk = require('chalk')
 
-const chalkUrl = "https://github.com/chalk/chalk"
+const chalkUrl = 'https://github.com/chalk/chalk'
 
 const log = console.log // Easy shortcut to console.log()
 
-// This prints out red-white-blue message of "Hello World, and Carl!" 
+// This prints out red-white-blue message of "Hello World, and Carl!"
 log(chalk.blue('Hello') + ' World' + chalk.red(', and Carl!'))
 
 // Compose multiple styles using the chainable API
-log(chalk.blue.bgRed.bold('Hello world!'));
+log(chalk.blue.bgRed.bold('Hello world!'))
 
 // Pass in multiple arguments
-log(chalk.blue('Hello', 'World!', 'Foo', 'bar', 'biz', 'baz'));
+log(chalk.blue('Hello', 'World!', 'Foo', 'bar', 'biz', 'baz'))
 
 // Nest styles
-log(chalk.red('Hello', chalk.underline.bgBlue('world') + '!'));
+log(chalk.red('Hello', chalk.underline.bgBlue('world') + '!'))
--- nodeReplStartup.js
+++ nodeReplStartup.js
@@ -1,8 +1,8 @@
 #!/usr/bin/env node
 
 // For interacting with nodejs repl
-const fs = require('fs-extra');
-const path = require('path');
-const os = require('os');
-const childproc = require('child_process');
-console.log('Nodejs repl ready!');
+const fs = require('fs-extra')
+const path = require('path')
+const os = require('os')
+const childproc = require('child_process')
+console.log('Nodejs repl ready!')
