cbebbf189459e92b1ad9ef2bf74dee90a49746e4
awsomesawce <awsomesawce@outlook.com>
2021-07-25 21:19:08 -0400

    Add temp files

---
+++ temp/nodeload.js
@@ -0,0 +1,13 @@
+const re = require('regexpp')
+const fs = require('fs-extra')
+const http2 = require('http2')
+const chalk = require('chalk')
+const process = require('process')
+const os = require('os')
+const _ = require('lodash')
+const mini = require('minimatch')
+//const esutils = require('esutils')
+const js-yaml = require('js-yaml')
+//const optionator = require('optionator')
+const sprintf = require('sprintf-js')
+sprintf
