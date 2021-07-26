af3b6aad1dd76e31fb299a6cd5ee541a0163a0da
Carl C <awsomesawce@outlook.com>
2021-07-25 21:05:09 -0400

    Update .psenv.ps1 file, install xo and markdown-it

--- .psenv.ps1
+++ .psenv.ps1
@@ -4,35 +4,36 @@
 # Functions relating to npm dev workflow
 
 function Page-Readme {
-<#
-.Syntax
-Page-Readme [npm-package-name]
-.Description
-Opens readme of any npm-installed package in node_modules folder, as long
-as it is "README.md"
-.NOTES
-Works like a pydoc for local nodejs projects
-Can be rewritten as a `foreach` loop.
+    <#
+    .Syntax
+    Page-Readme [npm-package-name]
+    .Description
+    Opens readme of any npm-installed package in node_modules folder, as long
+    as it is "README.md"
+    .NOTES
+    Works like a pydoc for local nodejs projects
+    Can be rewritten as a `foreach` loop.
 #>
-param([string[]]$dir)
-$x = "./node_modules/$dir/README.md"
-if ($dir) {
-    if (Test-Path $x) {
-        if (Get-Command bat -ErrorAction ignore) {
-	    # If bat pager is found use that, else use less.
-	    bat "$x"
-	} elseif (Get-Command less -ErrorAction ignore) {
-	    less "$x"
-	} else {
-	    Write-Verbose "Compatible pager program not found on PATH.  Using Powershell pager."
-	    Get-Content "$x" | out-host -Paging
+    param([string[]]$dir)
+    $x = "./node_modules/$dir/README.md"
+	if ($dir) {
+	    if (Test-Path $x) {
+		if (Get-Command bat -ErrorAction ignore) {
+		    # If bat pager is found use that, else use less.
+		    bat "$x"
+		} elseif (Get-Command less -ErrorAction ignore) {
+		    less "$x"
+		} else {
+		    Write-Verbose "Compatible pager program not found on PATH.  Using Powershell pager."
+		    Get-Content "$x" | out-host -Paging
+		}
+	    }
+	    else {
+		Write-Error "$x not found"
+	    }
+	}
+	else {
+	    Write-Error "Page-Readme needs an argument for it to page readmes.  Try again :)"
 	}
     }
-    else {
-        Write-Error "$x not found"
-    }
-}
-else {
-    Write-Error "Page-Readme needs an argument for it to page readmes.  Try again :)"
-}
-}
+
